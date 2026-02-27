939069ea5866c0e1a87f0550e6e24f053d1d18d0
udan-insith <udaninsith23613@gmail.com>
2026-02-27 07:20:36 +0530

    made some changes.

---
+++ app.js
@@ -0,0 +1,32 @@
+//                  SELECT ITEMS
+const alert = document.querySelector(".alert")
+const form = document.querySelector(".grocery-form")
+const grocery = document.getElementById("grocery")
+const submitBtn = document.querySelector(".submit-btn")
+const container = document.querySelector(".grocery-container")
+const list = document.querySelector(".grocery-list")
+const clearBtn = document.querySelector(".clear-btn")
+
+
+//                   EDIT OPTION
+let editElement
+let editFlag = false
+let editID = ""
+
+//                   EVENTLISTENERS
+//submit form
+form.addEventListener("submit", addItem)
+
+//                    FUNCTIONS
+function addItem(e) {
+    e.preventDefault()
+    const value = grocery.value
+    const id = new Date().getTime().toString()
+    if (value && !editFlag) {
+        console.log('add item to the list');
+    } else if (value && editFlag) {
+        console.log('editing');
+    } else {
+        console.log('empty value');
+    }
+}

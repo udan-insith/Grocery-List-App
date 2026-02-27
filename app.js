992a0e46be03650555c7e0abf5692f0e238f542f
udan-insith <udaninsith23613@gmail.com>
2026-02-27 07:47:53 +0530

    successfully completed adding items

--- app.js
+++ app.js
@@ -21,9 +21,32 @@ form.addEventListener("submit", addItem)
 function addItem(e) {
     e.preventDefault()
     const value = grocery.value
+
     const id = new Date().getTime().toString()
     if (value && !editFlag) {
-        console.log('add item to the list')
+        const element = document.createElement('article')
+        // add class
+        element.classList.add('grocery-item')
+        // add id
+        const attr = document.createAttribute('data-id')
+        attr.value = id
+        element.setAttributeNode(attr)
+        element.innerHTML = `
+        <p class="title">${value}</p>
+        <div class="btn-container">
+            <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+            </button>
+            <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+            </button>
+        </div>`
+        // append child
+        list.appendChild(element)
+        // display alert
+        displayAlert("item added to the list", "success")
+        // show container
+        container.classList.add("show-container")
     } else if (value && editFlag) {
         console.log('editing')
     } else {
